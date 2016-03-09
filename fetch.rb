40d734fd605fee583bfef8fb0bebf1193bd32530
Pedro Cavalheiro <pedrohc@gmail.com>
2016-03-09 14:18:06 -0300

    local wallpapers dir

--- fetch.rb
+++ fetch.rb
@@ -8,10 +8,10 @@ top_img_url = page.xpath("//*[@id='siteTable']/div[1]/div[2]/p[1]/a")[0]['href']
 
 time = Time.now.to_s.split(' ', 2).first
 
-system("mkdir -p ~/wallpapers")
+path = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
-system("gsettings set org.gnome.desktop.background picture-uri file:///home/pedro/wallpapers/null.jpg")
+system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/null.jpg")
 
-system("wget #{top_img_url} -O ~/wallpapers/#{time}.jpg")
+system("wget #{top_img_url} -O #{path}/wallpapers/#{time}.jpg")
 
-system("gsettings set org.gnome.desktop.background picture-uri file:///home/pedro/wallpapers/#{time}.jpg")
+system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/#{time}.jpg")
