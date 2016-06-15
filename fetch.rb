72e9999e962822489153f2699c94080fe01bd563
Pedro Cavalheiro <pedrohc@gmail.com>
2016-06-15 17:13:19 -0300

    fixes reddit api error (retry for the win!)

--- fetch.rb
+++ fetch.rb
@@ -4,16 +4,24 @@ require 'restclient'
 require 'json'
 require 'pp'
 
-json = JSON.parse(RestClient.get("https://www.reddit.com/r/EarthPorn/top.json?limit=1"))
+def good_resolution?(img)
+  img['width'] > 1600 && img['width'] > img['height']
+end
 
-top_img_url = json['data']['children'][0]['data']['preview']['images'][0]['source']['url']
+begin
+  results = JSON.parse(RestClient.get('https://www.reddit.com/r/EarthPorn/top.json?limit=10'))
+rescue
+  retry
+end
+
+top_img = results['data']['children'].detect { |i| good_resolution?(i['data']['preview']['images'][0]['source']) }
+
+img_url = top_img['data']['preview']['images'][0]['source']['url']
 
 time = Time.now.to_s.split(' ', 2).first
 
 path = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
 system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/null.jpg")
-
-system("wget #{top_img_url} -O #{path}/wallpapers/#{time}.jpg")
-
+system("wget #{img_url} -O #{path}/wallpapers/#{time}.jpg")
 system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/#{time}.jpg")
