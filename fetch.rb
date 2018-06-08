1eebc919c6f32eb62bd32f55c4708f906b925b46
adhithyan <38007128+adhithyan-4k@users.noreply.github.com>
2018-06-08 09:38:20 +0530

    Added macOS support

--- fetch.rb
+++ fetch.rb
@@ -5,6 +5,7 @@ require 'nokogiri'
 require 'restclient'
 require 'json'
 require 'pp'
+require 'open-uri'
 
 def good_resolution?(img)
   img['width'] > 1600 && img['width'] > img['height']
@@ -19,11 +20,20 @@ end
 top_img = results['data']['children'].detect { |i| good_resolution?(i['data']['preview']['images'][0]['source']) }
 
 img_url = top_img['data']['preview']['images'][0]['source']['url']
-
 time = Time.now.to_s.split(' ', 2).first
 
 path = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
-system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/null.jpg")
-system("wget #{img_url} -O #{path}/wallpapers/#{time}.jpg")
-system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/#{time}.jpg")
+OS = RUBY_PLATFORM
+if OS.include? "darwin"
+  path = "#{Dir.pwd}/wallpapers/#{time}.jpg"
+  open(path, 'wb') do |file|
+    file << open(img_url).read
+  end
+  set_wallpaper = "osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"#{path}\"'"
+  system(set_wallpaper)
+else
+  system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/null.jpg")
+  system("wget #{img_url} -O #{path}/wallpapers/#{time}.jpg")
+  system("gsettings set org.gnome.desktop.background picture-uri file://#{path}/wallpapers/#{time}.jpg")
+end
